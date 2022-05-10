d89729ef9e267eac5cd6ac4ea9e54354cb2c15c8
Samayamanthula Navya Sai Narayana Datta <samayamantula.saidatta@gmail.com>
2022-05-10 22:52:21 +0530

    Flask Setup

---
+++ jobs/app.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+"""
+hello_flask: First Python-Flask webapp
+"""
+from flask import Flask  # From module flask import class Flask
+from flask import render_template
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+
+@app.route('/')   # URL '/' to be handled by main() route handler
+@app.route('/jobs')
+
+
+def main():
+    """Say hello"""
+    return render_template('index.html')
+
+if __name__ == '__main__':  # Script executed directly?
+    app.run(debug=True) #  # Launch built-in web server and run this Flask webapp
